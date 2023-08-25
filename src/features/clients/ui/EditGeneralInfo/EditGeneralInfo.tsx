8a4929cbe47667d2a1b2c89f8fd52c1ec2f9a5c3
Alimzhanov Ibragim <ballismlife@gmail.com>
2023-08-25 12:51:40 +0500

    feat: editGeneralInfo for leads and clients

--- src/features/clients/ui/EditGeneralInfo/EditGeneralInfo.tsx
+++ src/features/clients/ui/EditGeneralInfo/EditGeneralInfo.tsx
@@ -1,25 +1,34 @@
 import cn from 'classnames';
 import { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 
 import { ClientsGeneralInfo } from 'entities/clients';
-import { Button, Modal, Icon } from 'shared/ui';
+import { Button, Modal, Icon, Input } from 'shared/ui';
 
 type IFormInput = {
-  firstName: string;
-  lastName: string;
-  iceCreamType: { label: string; value: string };
+  address: string;
+  docNumber: string;
+  nationality: string;
+  countryOfIssue: string;
+  placeOfBirth: string;
+  dateOfBirth: string;
+  dateOfPassportCreation: string;
+  dateOfPassportExpiry: string;
+  passport: string;
 };
 
 export const EditGeneralInfo = () => {
   const { control, handleSubmit } = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      iceCreamType: {
-        label: '',
-        value: '',
-      },
+      address: '',
+      docNumber: '',
+      nationality: '',
+      countryOfIssue: '',
+      placeOfBirth: '',
+      dateOfBirth: '',
+      dateOfPassportCreation: '',
+      dateOfPassportExpiry: '',
+      passport: '',
     },
   });
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
@@ -50,20 +59,6 @@ export const EditGeneralInfo = () => {
             title='Edit general information'
             botSlot={
               <div className={cn('flex justify-between items-center w-full')}>
-                {false && (
-                  <Button
-                    className='w-full !text-gray-500 w-auto hover:bg-gray-100'
-                    typeStyle='link'
-                    color='dark'
-                    size='default'
-                    onClick={handleClose}
-                    leadingIconClassName='!p-0 w-[14px] h-[14px]'
-                    leadingIcon='chevron-left'
-                    leadingIconColor='gray'
-                  >
-                    Previous step
-                  </Button>
-                )}
                 <div className={cn('flex items-center gap-[10px] ml-auto')}>
                   <Button
                     className='w-full'
@@ -77,17 +72,174 @@ export const EditGeneralInfo = () => {
                   <Button
                     className='w-full'
                     size='default'
-                    trailingIcon='chevron-right'
-                    trailingIconClassName='!p-0 w-[14px] h-[14px]'
-                    trailingIconColor='light'
+                    // trailingIcon='chevron-right'
+                    // trailingIconClassName='!p-0 w-[14px] h-[14px]'
+                    // trailingIconColor='light'
                   >
-                    Next
+                    Apply
                   </Button>
                 </div>
               </div>
             }
           >
-            <form onSubmit={handleSubmit(onSubmit)}></form>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className={cn('grid grid-cols-2 gap-x-[15px] gap-y-[20px]')}
+            >
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Address'
+                    placeholder='Address'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='address'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Document Number'
+                    placeholder='Document Number'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='docNumber'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Nationality'
+                    placeholder='Nationality'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='nationality'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Country Of Issue'
+                    placeholder='Country Of Issue'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='countryOfIssue'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Place of Birth'
+                    placeholder='Place of Birth'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='placeOfBirth'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Date of Birth'
+                    placeholder='Date of Birth'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='dateOfBirth'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Date of Passport Creation'
+                    placeholder='Date of Passport Creation'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='dateOfPassportCreation'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Date Of Passport Expiry'
+                    placeholder='Date Of Passport Expiry'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='dateOfPassportExpiry'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Passport'
+                    placeholder='Passport'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='passport'
+                control={control}
+                rules={{ required: true }}
+              />
+            </form>
           </Modal>
         </>
       }
