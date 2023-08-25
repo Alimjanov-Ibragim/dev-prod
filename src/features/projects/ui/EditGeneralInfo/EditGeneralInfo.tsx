846c3c2f3bff69a4f69e26c1c0526b0b0953c4fd
Alimzhanov Ibragim <ballismlife@gmail.com>
2023-08-25 13:33:22 +0500

    feat: for projects added form fields for editGeneralInfo

--- src/features/projects/ui/EditGeneralInfo/EditGeneralInfo.tsx
+++ src/features/projects/ui/EditGeneralInfo/EditGeneralInfo.tsx
@@ -1,25 +1,42 @@
 import cn from 'classnames';
 import { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 
 import { ProjectsGeneralInfo } from 'entities/projects';
-import { Button, Modal, Icon } from 'shared/ui';
+import { Button, Modal, Icon, Input } from 'shared/ui';
 
 type IFormInput = {
-  firstName: string;
-  lastName: string;
-  iceCreamType: { label: string; value: string };
+  developer: string;
+  community: string;
+  address: string;
+  estimatedCompletionDate: string;
+  blockable: string;
+  brokerAgencyShare: string;
+  agentShareForCompanyLead: string;
+  agentShareForPersonalLead: string;
+  bankName: string;
+  swiftCode: string;
+  corporateAccount: string;
+  escrowAccount: string;
+  ibanAccount: string;
 };
 
 export const EditGeneralInfo = () => {
   const { control, handleSubmit } = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      iceCreamType: {
-        label: '',
-        value: '',
-      },
+      developer: '',
+      community: '',
+      address: '',
+      estimatedCompletionDate: '',
+      blockable: '',
+      brokerAgencyShare: '',
+      agentShareForCompanyLead: '',
+      agentShareForPersonalLead: '',
+      bankName: '',
+      swiftCode: '',
+      corporateAccount: '',
+      escrowAccount: '',
+      ibanAccount: '',
     },
   });
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
@@ -50,20 +67,6 @@ export const EditGeneralInfo = () => {
             title='Edit general information'
             botSlot={
               <div className={cn('flex justify-between items-center w-full')}>
-                {false && (
-                  <Button
-                    className='w-full !text-gray-500 w-auto hover:bg-gray-100'
-                    typeStyle='link'
-                    color='dark'
-                    size='default'
-                    onClick={handleClose}
-                    leadingIconClassName='!p-0 w-[14px] h-[14px]'
-                    leadingIcon='chevron-left'
-                    leadingIconColor='gray'
-                  >
-                    Previous step
-                  </Button>
-                )}
                 <div className={cn('flex items-center gap-[10px] ml-auto')}>
                   <Button
                     className='w-full'
@@ -77,17 +80,242 @@ export const EditGeneralInfo = () => {
                   <Button
                     className='w-full'
                     size='default'
-                    trailingIcon='chevron-right'
-                    trailingIconClassName='!p-0 w-[14px] h-[14px]'
-                    trailingIconColor='light'
+                    // trailingIcon='chevron-right'
+                    // trailingIconClassName='!p-0 w-[14px] h-[14px]'
+                    // trailingIconColor='light'
                   >
-                    Next
+                    Apply
                   </Button>
                 </div>
               </div>
             }
           >
-            <form onSubmit={handleSubmit(onSubmit)}></form>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className={cn('grid grid-cols-2 gap-x-[15px] gap-y-[20px]')}
+            >
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Developer'
+                    placeholder='Developer'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='developer'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Community'
+                    placeholder='Community'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='community'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Address'
+                    placeholder='Address'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='address'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Estimated Completion Date'
+                    placeholder='Estimated Completion Date'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='estimatedCompletionDate'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Blockable'
+                    placeholder='Blockable'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='blockable'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Broker Agency share'
+                    placeholder='Broker Agency share'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='brokerAgencyShare'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Agent Share For Company Lead'
+                    placeholder='Agent Share For Company Lead'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='agentShareForCompanyLead'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Agent Share For Personal Lead'
+                    placeholder='Agent Share For Personal Lead'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='agentShareForPersonalLead'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Bank name'
+                    placeholder='Bank name'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='bankName'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Swift code'
+                    placeholder='Swift code'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='swiftCode'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Corporate account'
+                    placeholder='Corporate account'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='corporateAccount'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Escrow Account'
+                    placeholder='Escrow Account'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='escrowAccount'
+                control={control}
+                rules={{ required: true }}
+              />
+              <Controller
+                render={({
+                  field: { onChange, onBlur, value, name, ref },
+                  fieldState: { invalid, isTouched, isDirty, error },
+                }) => (
+                  <Input
+                    label='Iban Account'
+                    placeholder='Iban Account'
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                  />
+                )}
+                name='ibanAccount'
+                control={control}
+                rules={{ required: true }}
+              />
+            </form>
           </Modal>
         </>
       }
